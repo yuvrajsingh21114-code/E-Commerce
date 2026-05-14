e81127a962592a70374f6033a7084f19c6f12f63
Yuvraj Singh <yuvrajsingh21114@gmail.com>
2026-05-14 13:40:37 +0530

    added connection with server to fetch data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,37 @@
+import axios from 'axios'
+import { useState,useEffect } from 'react'
 import { MenuBar } from '../components/MenuBar'
 import { Product } from '../components/Product'
 import './HomePage.css'
-import img1 from '../assets/speaker.png'
 
 export function HomePage() {
+    const [products,setProducts]=useState([]);
+
+    useEffect(()=>{
+        async function fetchProducts(){
+            try{
+                const response=await axios.get('http://localhost:5000/products');
+                setProducts(response.data);
+            }
+            catch(error){
+                console.log(error);
+            }
+        }
+
+        fetchProducts();
+    },[]);
 
     return(
-        <>
+        <>  
             <MenuBar />
-            <div className="Products-Container">
-                <Product name="Bluetooth speaker with Aux and Sub Woofer" loc={img1} />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-            </div>
             
+            <div className="Products-Container">
+                {products.map((product)=>{
+                    return(
+                    <Product name={product.name} />
+                    )
+                })}
+            </div>            
         </>        
     );
 

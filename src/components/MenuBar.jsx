91ff28d0994595d9eaf2cd3bc3c68759a70ad31d
Yuvraj Singh <yuvrajsingh21114@gmail.com>
2026-06-13 13:29:39 +0530

    added search box suggestions

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,13 +1,34 @@
 import { Link } from 'react-router';
 import './MenuBar.css';
+import { useState } from 'react';
 
 export function MenuBar() {
+    const[show,setShow]=useState(false);
+    function focus(value){
+        console.log(value);
+    }
+
     return (
         <>
             <div className="MenuBar-Container">
                 <Link to="/" className="MenuBar-Text">E-Commerce</Link>
                 <div className="SearchBox-Container">
-                    <input className="MenuBar-SearchBox" placeholder="Enter to Search"></input>
+                    <input className="MenuBar-SearchBox" 
+                        placeholder="Enter to Search" 
+                        onFocus={()=>{setShow('grid')}} 
+                        onBlur={()=>{setTimeout(() => {
+                            setShow(false)
+                        }, 500);}}></input>
+                    <div className="Search-menu-box" style={{display:show}}>
+                        <div className="Search-menu-suggestion"  onClick={()=>{focus("s1")}}><label>Suggestion1</label></div>
+                        <div className="Search-menu-suggestion"  onClick={()=>{focus("s1")}}><label>Suggestion1</label></div>
+                        <div className="Search-menu-suggestion"  onClick={()=>{focus("s1")}}><label>Suggestion1</label></div>
+                        <div className="Search-menu-suggestion"  onClick={()=>{focus("s1")}}><label>Suggestion1</label></div>
+                        <div className="Search-menu-suggestion"  onClick={()=>{focus("s1")}}><label>Suggestion1</label></div>
+                        <div className="Search-menu-suggestion"  onClick={()=>{focus("s1")}}><label>Suggestion1</label></div>
+                        <div className="Search-menu-suggestion"  onClick={()=>{focus("s1")}}><label>Suggestion1</label></div>
+                        <div className="Search-menu-suggestion"  onClick={()=>{focus("s1")}}><label>Suggestion1</label></div>
+                    </div>
                     <Link className="MenuBar-SearchButton">Search</Link>
                 </div>
 
@@ -17,9 +38,7 @@ export function MenuBar() {
                 </div>
             </div>
 
-            <div className="Search-menu-box">
-                akldjflakj
-            </div>
+
         </>
     );
 }
